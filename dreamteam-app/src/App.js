6ca5a82b205fa387a994a3a6196841de7697abb5
shkshitiz <shkshitiz1@gmail.com>
2022-10-25 16:18:52 +1030

    initialize commit

---
+++ dreamteam-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dream Team</h1>
+        <form action="">
+
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;

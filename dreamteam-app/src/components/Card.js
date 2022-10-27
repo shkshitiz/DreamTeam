8a66ea8228db4c6efcec280bff72419ae5c9272e
shkshitiz <shkshitiz1@gmail.com>
2022-10-28 09:28:31 +1030

    add outcome of the game

--- dreamteam-app/src/components/Card.js
+++ dreamteam-app/src/components/Card.js
@@ -6,6 +6,12 @@ import LineChart from "./LineChart";
 
 function Card(gameIndex) {
   const game = gameIndex.gameIndex
+  let result
+  if (game.win == true) {
+    result = 'Win'
+  } else {
+    result = 'Loss'
+  }
   
   return(
     <div className="Card">
@@ -13,6 +19,7 @@ function Card(gameIndex) {
       <img src={Fetcher.championCard(game.championName)} alt='champion'/>
       <p>{game.kills}/{game.deaths}/{game.assists}</p>
       <p>{(game.kills + game.assists) / game.deaths}:1 KDA</p>
+      <p>{result}</p>
       <div className="chart">
         <canvas>
           <BarChart />
